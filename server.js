26a556b1d03b5166b493661a3e04189257f7f441
Runfire22 <cwn951@live.dk>
2019-04-07 10:45:54 +0200

    Tilføjede Schema

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server has started on ort ${port}`));
+app.listen(port, () => console.log(`Server has started on port ${port}`));
